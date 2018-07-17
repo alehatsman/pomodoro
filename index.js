f6127dded600c654f82420aa32756eb83b8c2a2e
Astma <aleh.atsman@gmail.com>
2018-07-17 10:13:17 +0200

    Dont open devtools, asar, packager

--- index.js
+++ index.js
@@ -16,9 +16,6 @@ function createWindow () {
   // and load the index.html of the app.
   win.loadFile('index.html')
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
-
   // Emitted when the window is closed.
   win.on('closed', () => {
     // Dereference the window object, usually you would store windows
